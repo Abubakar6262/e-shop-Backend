110f4318d0a4e8defa313ec9319bdfc082e34843
abubakar <abubkarmulhtar44@gmail.com>
2024-09-04 14:54:41 +0500

    change in send token

--- utils/shopToken.js
+++ utils/shopToken.js
@@ -1,19 +1,14 @@
-// create Token and saving that in cookies
-
 const sendShopToken = (user, res) => {
     const token = user.getJwtToken();
 
-    // options for cookies
     const options = {
-        expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000), // 90 days from now
+        expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000), // 90 days
         httpOnly: true,
-        // new change after deployment
-        secure: process.env.NODE_ENV === 'production', // Only use Secure cookies in production
-        sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'Lax', // SameSite=None required for cross-site cookies in production
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'Lax',
         path: '/',
-
     };
-    // console.log("token after user send ", token);
+
     return res.status(200)
         .cookie("seller_token", token, options)
         .json({ user, token, message: "User activated successfully" });
